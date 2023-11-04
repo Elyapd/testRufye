ca865a017c9ae4fc0713e0b34926a92e36dce80b
Elya <ealina2008@icloud.com>
2023-11-04 10:45:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+
+app = QApplication([])
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.show()
+
+mw = MainWindow()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.exec()
