3d09f315784f21fb9c53471c005d4148c0c5cd74
Elya <ealina2008@icloud.com>
2023-11-04 11:36:18 +0300

    changed window title

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
 app = QApplication([])
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.set_appear()
+        self.init_ui()
+        self.connects()
+    def set_appear(self):
         self.show()
+        self.setWindowTitle('test Rufye')
+    def init_ui(self):
+        label = QLabel('Добро пожаловать!')
+        vline =
+    def connects(self):
+        pass
 
 mw = MainWindow()
-mw.setWindowTitle('test Rufye')
+
 
 
 
